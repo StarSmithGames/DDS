ee3f758ecf1d475f66741fe39cd0b91c6da6f356
Davidovich Igor Alexandrovich <sir.funtik18@gmail.com>
2022-01-21 19:03:09 +0700

    check

--- Assets/Game/Scripts/UI/RadialMenu/RadialMenu.cs
+++ Assets/Game/Scripts/UI/RadialMenu/RadialMenu.cs
@@ -3,25 +3,29 @@ using Sirenix.OdinInspector;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEditor;
 
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 using static UnityEditor.Progress;
 
 public class RadialMenu : MonoBehaviour
 {
     [SerializeField] private Transform parent;
+    [SerializeField] private RadialMenuCursor cursor;
+    [SerializeField] private bool isSnap = true;
     [Range(1, 12)]
     [OnValueChanged("UpdateMenu")]
     [SerializeField] private int count;
-    [SerializeField] private GameObject prefab;
+    [SerializeField] private RadialMenuOption prefab;
 
-    private List<GameObject> options = new List<GameObject>();
+    private List<RadialMenuOption> options = new List<RadialMenuOption>();
 
     [OnValueChanged("UpdateRadius")]
-    public float radius = 150f;
+    public float maxRadius = 200f;
 
     public void OpenMenu()
     {
@@ -38,6 +42,34 @@ public class RadialMenu : MonoBehaviour
         OpenMenu();
     }
 
+    private void Update()
+    {
+        Vector3 screenBounds = new Vector3((float)Screen.width / 2f, (float)Screen.height / 2f, 0f);
+        Vector3 vector = Input.mousePosition - screenBounds;
+
+        float mouseRotation = Mathf.Atan2(vector.x, vector.y) * 57.29578f;
+        if (mouseRotation < 0f)
+            mouseRotation += 360f;
+        float cursorRotation = -(mouseRotation - cursor.FillAmount * 180f);
+
+        float difference = 9999;
+        RadialMenuOption nearest = null;
+        for (int i = 0; i < options.Count; i++)
+        {
+            float rotation = options[i].Rotation;
+
+            if (Mathf.Abs(rotation - mouseRotation) < difference)
+            {
+                nearest = options[i];
+                difference = Mathf.Abs(rotation - mouseRotation);
+            }
+        }
+
+		if (isSnap)
+			cursorRotation = -(nearest.Rotation - cursor.FillAmount * 360f / 2f);
+		cursor.SetRotation(Quaternion.Euler(0, 0, cursorRotation));//Quaternion.Slerp(cursor.transform.localRotation, , lerpAmount));
+    }
+
 	public void UpdateMenu()
     {
         ReSizeOptions();
@@ -47,21 +79,30 @@ public class RadialMenu : MonoBehaviour
 
 	private void UpdateRadius()
 	{
-        float fillRadius = (1f / (float)count) * 360f;
-        float previousRotation = 0;
+        if (!Application.isPlaying) return;
+
+        float normalCount = (1f / (float)count);
+
+        float fillRadius = normalCount * 360f;
+        cursor.FillAmount = normalCount;
+
         //y=sin(angle)
         //x=cos(angle)
+        float prevRotation = 0;
         for (int i = 0; i < options.Count; i++)
         {
-            float bRot = previousRotation + fillRadius / 2;
-            previousRotation = bRot + fillRadius / 2;
+            float rotation = prevRotation + fillRadius / 2;
+            prevRotation = rotation + fillRadius / 2;
 
-            options[i].transform.localPosition = new Vector2(radius * Mathf.Cos((bRot - 90) * Mathf.Deg2Rad), -radius * Mathf.Sin((bRot - 90) * Mathf.Deg2Rad));
+            options[i].Rotation = rotation;
+            options[i].SetPosition(new Vector2(maxRadius * Mathf.Cos((rotation - 90) * Mathf.Deg2Rad), -maxRadius * Mathf.Sin((rotation - 90) * Mathf.Deg2Rad)));
         }
 	}
 
     private void ReSizeOptions()
 	{
+        if (!Application.isPlaying) return;
+
         int diff = options.Count - count;
 
         if (diff > 0)
@@ -70,19 +111,19 @@ public class RadialMenu : MonoBehaviour
 			{
                 var go = options[i];
                 options.Remove(go);
-                Destroy(go);
+                Destroy(go.gameObject);
 			}
         }
         else
         {
             for (int i = 0; i < -diff; i++)
             {
-                Transform option = Instantiate(prefab).transform;
-                option.SetParent(parent);
-                option.position = Vector3.zero;
-                option.localScale = Vector3.one;
+                RadialMenuOption option = Instantiate(prefab);
+                option.transform.SetParent(parent);
+                option.transform.position = Vector3.zero;
+                option.transform.localScale = Vector3.one;
 
-                options.Add(option.gameObject);
+                options.Add(option);
             }
         }
     }
